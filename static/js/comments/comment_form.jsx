5c52758882b29e68d445ed389a9b3ecf33f8c6eb
Steve Penland <steve@ospreydata.com>
2015-12-13 13:51:20 -0800

    jQuery tests

--- static/js/comments/comment_form.jsx
+++ static/js/comments/comment_form.jsx
@@ -28,11 +28,65 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm" className="commentForm" onSubmit={this.handleSubmit}>
         <form className="commentForm">
-            <input
-            type="text"
-            placeholder="Playbook Name"
-            value={this.state.playbook_name}
-            onChange={this.handleNameChange} />
+            <div className="form-group">
+                <label htmlFor="color" className="control-label">
+                    Chart Color*
+                </label>
+                <div className="select-color">
+                    <input id="color" name="color" type="text"
+                           className="required"
+                           value=""/>
+                    <label htmlFor="sensor-def-color">Select</label>
+                </div>
+            </div>
+            <div className="form-group inline clearfix">
+                <label>
+                    <input type="checkbox"
+                           Nameid="mon" />
+                    Mon
+                </label>
+                <div className='input-group datepicker'>
+                    <input id=""
+                           name=""
+                           className="form-control required"
+                           type="text"
+                           placeholder="09:00:00" />
+                    <span className="input-group-addon">
+                        <span className="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+                <p>to</p>
+                <div className='input-group datepicker'>
+                    <input id=""
+                           name=""
+                           className="form-control required"
+                           type="text"
+                           placeholder="17:00:00" />
+                    <span className="input-group-addon">
+                        <span className="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label className="control-label">Urgency</label>
+                <select id="escalation-urgency"
+                        className="form-control selectpicker required"
+                        name="escalation-urgency" title="Select">
+                    <option value="24">24 hour</option>
+                    <option value="custom">Custom</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label className="control-label">Name</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Playbook Name"
+                    value={this.state.playbook_name}
+                    onChange={this.handleNameChange} />
+            </div>
+            
           <input
             type="text"
             placeholder="Your name"
